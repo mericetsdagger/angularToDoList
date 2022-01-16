7edee34f926085d5cc07e13741d45c3c2899e8fd
mericetsdagger <papaj.jacek@gmail.com>
2022-01-16 19:21:54 +0100

    zapis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Task } from './task';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularTemplate';
+
+  getFooter(): string {
+    return 'footer string';
+  }
+
+  clearTasks(): void {
+    this.tasks = [];
+    alert("zadania zostaly wyczyszczone");
+  }
+
+  tasks: Task[] = [
+    {
+      name: 'task1',
+      deadline: '2022-01-16',
+      done: false
+    },
+    {
+      name: 'task2',
+      deadline: '2022-02-01',
+      done: false
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
+import { registerLocaleData } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import localePl from '@angular/common/locales/pl';
 
 import { AppComponent } from './app.component';
+
+registerLocaleData(localePl);
 
 @NgModule({
   declarations: [
